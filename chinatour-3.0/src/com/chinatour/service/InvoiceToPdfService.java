714c4263195b15c4d33234ef4297756de52860b4
cfy202 <cfy871222@163.com>
2019-01-18 12:01:11 +0800

    erp

---
+++ chinatour-3.0/src/com/chinatour/service/InvoiceToPdfService.java
@@ -0,0 +1,27 @@
+package com.chinatour.service;
+
+public interface InvoiceToPdfService {
+	//总单确认单
+	String createInvoicePdf(String orderId,String logo);
+	
+	//子单确认单
+	String createInvoicePdfForChild(String ordersTotalId);
+	
+	String CreateInvoicePdfForTicket(String id);
+	
+	//给自组订单的invoice
+	public String createInvoiceForSelf(String ordersTotalId,String logo);
+	
+	//自组子单
+	public String createSelfInvoicePdfForChild(String orderId);
+	
+	//同行订单invoice
+	String createInvoicePdfForVender(String orderId);
+	//B2B Invoice(总单)
+	String createInvoicePdfB(String orderId,String logo);
+
+	String CreateInvoicePdfForRevise(String orderId);
+	
+	String CreateInvoicePdfForReviseWholeSale(String orderId);
+	
+}
